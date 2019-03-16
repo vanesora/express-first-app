07fe51496c954bc4808c2d2f0ac34c06288856b7
Jess Casas <jlcasasg@inkrementaldev.co>
2019-03-15 20:40:34 -0500

    Add configs

--- models/user.model.js
+++ models/user.model.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
     var Model = sequelize.define('User', {
         first     : DataTypes.STRING,
         last      : DataTypes.STRING,
-        email     : {type: DataTypes.STRING, allowNull: true, unique: true, validate: { isEmail: {msg: "Phone number invalid."} }},
+        email     : {type: DataTypes.STRING, allowNull: true, unique: true, validate: { isEmail: {msg: "Email invalid."} }},
         phone     : {type: DataTypes.STRING, allowNull: true, unique: true, validate: { len: {args: [7, 20], msg: "Phone number invalid, too short."}, isNumeric: { msg: "not a valid phone number."} }},
         password  : DataTypes.STRING,
     });
